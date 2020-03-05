4938c6e16fae0a7421cdcc687518595bb72d9bc6
Rodrigo Bianchini <rodrigo.bianchinii@gmail.com>
2020-03-05 14:51:56 -0300

    upload site

---
+++ 404.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+
+    <!-- fontes -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Sarabun" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+
+    <!-- favicons
+    ================================================== -->
+    <link rel="icon" href="../app/img/favicon.png"/>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+
+    <link href="../app/css/estilo.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/solid.css" integrity="sha384-+0VIRx+yz1WBcCTXBkVQYIBVNEFH1eP6Zknm16roZCyeNg2maWEpk/l/KsyFKs7G" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/fontawesome.css" integrity="sha384-jLuaxTTBR42U2qJ/pm4JRouHkEDHkVqH0T1nyQXn1mZ7Snycpf6Rl25VBNthU4z0" crossorigin="anonymous">
+
+
+    <title>eCard</title>
+
+    <meta name="author" content="eCard">
+    <meta name="description" content="">
+
+
+  	<style type="text/css">
+  		body{
+  			background-color: #0072e4;
+		} 
+
+		h1{
+			font-size: 40px;
+		}
+
+		h2{
+			font-size: 20px;
+		}
+
+		a{
+			color: #aff1ff;
+		}
+
+		a:hover{
+			color: #00d2ff;
+		}
+
+  	</style>
+
+
+
+  </head>
+  <body>
+
+<!--     <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#"><img src="../app/img/logo-hr-ico.png" class="logonav"></a>
+    </nav> -->
+
+    <br>
+
+
+    <div class="container"> 
+        <div class="row"> 
+            <div class="col-sm-12 col-md-10 centered"> <br><br>
+              <div style="text-align: center; color: #fff; font-family: 'Raleway', sans-serif;">
+
+              	<img src="images/logo-bco.png">
+              	<h1>Ooops!</h1>
+              	<h2>Este cartão não foi encontrado.</h2>
+              	<br><br>
+
+              	<p>Mas a boa notícia é que ele <a href="index.html#price_page">pode ser seu!</a></p>
+              </div>
+        </div>
+    </div>  
+    
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
